5eb3af8f03de8ef395df93bbd09054037e971abd
Aymen Bouazizi <aymendotbouazizi@gmail.com>
2025-03-08 12:15:33 +0100

    Finishing server setup and added CORS config

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8081",
+// CORS options
+const corsOptions = {
+  origin: "http://localhost:4200", // or '*' if you want to allow all origins
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, // Allow credentials (cookies or HTTP authentication)
 };
 
+// Enable CORS with options
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
@@ -20,8 +25,38 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to aymen's application." });
 });
 
+// setting routes
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+// init the database
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and Resync Db");
+  initial();
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user",
+  });
+
+  Role.create({
+    id: 2,
+    name: "moderator",
+  });
+
+  Role.create({
+    id: 3,
+    name: "admin",
+  });
+}
